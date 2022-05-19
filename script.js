adad4757b9f447d713d45109948e43abaebcb5b8
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2022-05-19 08:27:57 -0500

    Added JavaScript Variables

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 "use strict";
 
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
 
+function showError(input, message) {
+
+    
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (username.value === "") {
+    showError(username, "Please enter a username");
+  } else {
+    showSuccess(username);
+  }
+});
